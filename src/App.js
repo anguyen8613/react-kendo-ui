67f956ff7988c6d4a68cce2a20ce7f87e80d1a23
Alvin <anguyen8613@gmail.com>
2020-02-03 15:17:44 -0800

    Setup kendo ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react';
+import { process } from '@progress/kendo-data-query';
+import { Grid, GridColumn } from '@progress/kendo-react-grid';
+import { DropDownList } from '@progress/kendo-react-dropdowns';
+import { Window } from '@progress/kendo-react-dialogs';
+import '@progress/kendo-theme-default/dist/all.css';
 
 
 class App extends Component {
